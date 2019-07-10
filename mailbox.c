b6dd1e17bd6b65b67e7633bf0b08ef09ae07fb55
MCMrARM <mcmrarm@gmail.com>
2019-07-10 14:00:14 +0200

    Fix register locs

--- mailbox.c
+++ mailbox.c
@@ -2,6 +2,10 @@
 #include <linux/atomic.h>
 #include "pci.h"
 
+#define REG_MBOX_OUT_BASE 0x820
+#define REG_MBOX_REPLY_COUNTER 0x108
+#define REG_MBOX_REPLY_BASE 0x810
+
 void bce_mailbox_init(struct bce_mailbox *mb, void __iomem *reg_mb)
 {
     mb->reg_mb = reg_mb;
@@ -10,7 +14,7 @@ void bce_mailbox_init(struct bce_mailbox *mb, void __iomem *reg_mb)
 
 int bce_mailbox_send(struct bce_mailbox *mb, u64 msg, u64* recv)
 {
-    void __iomem *regb;
+    u32 __iomem *regb;
 
     if (atomic_cmpxchg(&mb->mb_taken, 0, 1) != 0) {
         return -EEXIST; // We don't support two messages at once
@@ -18,7 +22,7 @@ int bce_mailbox_send(struct bce_mailbox *mb, u64 msg, u64* recv)
     reinit_completion(&mb->mb_completion);
 
     pr_debug("bce_mailbox_send: %llx", msg);
-    regb = mb->reg_mb + 0x208;
+    regb = (u32*) ((u8*) mb->reg_mb + REG_MBOX_OUT_BASE);
     iowrite32((u32) msg, regb);
     iowrite32((u32) (msg >> 32), regb + 1);
     iowrite32(0, regb + 2);
@@ -34,13 +38,13 @@ int bce_mailbox_send(struct bce_mailbox *mb, u64 msg, u64* recv)
 
 static int bce_mailbox_retrive_response(struct bce_mailbox *mb)
 {
-    void __iomem *regb;
+    u32 __iomem *regb;
     u32 lo, hi;
     int count, counter;
-    count = ioread32(mb->reg_mb + 0x42);
+    count = ioread32((u8*) mb->reg_mb + REG_MBOX_REPLY_COUNTER);
     counter = count;
     while (counter--) {
-        regb = mb->reg_mb + 0x204;
+        regb = (u32*) ((u8*) mb->reg_mb + REG_MBOX_REPLY_BASE);
         lo = ioread32(regb);
         hi = ioread32(regb + 1);
         ioread32(regb + 2);
